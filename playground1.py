569db1b80d602071494ad508b527e84327adc897
HyunsungRyu <seal0130@gmail.com>
2025-06-11 20:53:43 +0900

    haha it works, too

--- playground1.py
+++ playground1.py
@@ -1,28 +1,45 @@
 import os
 from pyspark import SparkContext, RDD
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
 
 os.environ["PYSPARK_PYTHON"] = "C:/git_files/pyspark-playground/venv/Scripts/python.exe"
 os.environ["PYSPARK_DRIVER_PYTHON"] = "C:/git_files/pyspark-playground/venv/Scripts/python.exe"
 
 if __name__ == "__main__":
 
+# case 1
+    # ss: SparkSession = SparkSession.builder \
+    #     .master("local[*]") \
+    #     .appName("spark RDD") \
+    #     .getOrCreate()
+
+    # sc: SparkContext = ss.sparkContext
+
+    # text_file: RDD[str] = sc.textFile("MultidomainSituationalAssessmentReport1.txt")
+
+    # counts = text_file \
+    #     .flatMap(lambda line: line.split(" ")) \
+    #     .map(lambda word: (word, 1)) \
+    #     .reduceByKey(lambda a, b: a + b)
+
+    # output = counts.collect()
+    # for word, count in output:
+    #     print(f"{word}: {count}")
+
+    # ss.stop()
+
+
+# case 2
     ss: SparkSession = SparkSession.builder \
-        .master("local[*]") \
-        .appName("playground1") \
-        .getOrCreate()
-
-    sc: SparkContext = ss.sparkContext
-
-    text_file: RDD[str] = sc.textFile("MultidomainSituationalAssessmentReport1.txt")
-
-    counts = text_file \
-        .flatMap(lambda line: line.split(" ")) \
-        .map(lambda word: (word, 1)) \
-        .reduceByKey(lambda a, b: a + b)
-
-    output = counts.collect()
-    for word, count in output:
-        print(f"{word}: {count}")
-
-    ss.stop()
+    .master("local[*]") \
+    .appName("spark sql") \
+    .getOrCreate()
+    
+    df = ss.read.text("MultidomainSituationalAssessmentReport2.txt")
+    
+    df = df.withColumn('word', f.explode(f.split(f.col('value'), " "))) \
+    .withColumn("count", f.lit(1)).groupby("word").sum()
+    
+    df.show()
+    

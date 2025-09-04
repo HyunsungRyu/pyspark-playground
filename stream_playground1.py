1e9e5e33b0c51448117d353d356f9e2f96cd90bf
HyunsungRyu <seal0130@gmail.com>
2025-09-04 20:32:31 +0900

    stream

---
+++ stream_playground1.py
@@ -0,0 +1,3 @@
+import os
+os.environ["PYSPARK_PYTHON"] = "C:/git_files/pyspark-playground/venv/Scripts/python.exe"
+os.environ["PYSPARK_DRIVER_PYTHON"] = "C:/git_files/pyspark-playground/venv/Scripts/python.exe"
